d8a815d64d49338ffe8532742fa10bbb1a266c5e
Jeremy Desiderio <jdesiderio@supplyforce.com>
2024-09-30 13:45:58 -0400

    Created child class.

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ class Car:
         self.color = color
 
 
+class Mfg(Car):
+    def __init__(self, wheels, color, name):
+        super().__init__(wheels, color)
+        self.name = name
+
+
+
+
 
 if __name__ == '__main__':
     test_car = Car(4, 'red')
     print(f'This car has {test_car.wheels} wheels')
+    test_mfg = Mfg(4, 'green', 'Ford')
+    print(f'This car was made by {test_mfg.name} and is the color {test_mfg.color}.')
+
+
     
 
