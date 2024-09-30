16ed8fb6d26c3e9853df7e88dc7c9b8d11fcfefc
Jeremy Desiderio <jdesiderio@supplyforce.com>
2024-09-30 14:40:20 -0400

    created parent class

--- instrument.py
+++ instrument.py
@@ -1,3 +1,25 @@
+class Instrument():
+    def __init__(self, played, sound, timbre):
+        self.played = played
+        self.sound = sound
+        self.timbre = timbre
+
+    def play_instrument(self):
+        return f'Instruments are played {self.played}, produce a {self.sound} sound, and have a {self.timbre} timbre.'
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     print("Hello world")
+    test_intrument = Instrument('loudly', 'melodious', 'different')
+    print(test_intrument.play_instrument())
+    
+
+
+
 
