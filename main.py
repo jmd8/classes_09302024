8bbaa1fe0d5255f018b5b2019f12b61d5b5d5964
Jeremy Desiderio <jdesiderio@supplyforce.com>
2024-09-30 13:53:09 -0400

    Added method.

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ class Car:
         self.wheels = wheels
         self.color = color
 
+    def summary(self):
+        return f'This car has {test_car.wheels} wheels'
+
 
 class Mfg(Car):
     def __init__(self, wheels, color, name):
@@ -15,10 +18,11 @@ class Mfg(Car):
 
 if __name__ == '__main__':
     test_car = Car(4, 'red')
-    print(f'This car has {test_car.wheels} wheels')
+    print(test_car.summary())
     test_mfg = Mfg(4, 'green', 'Ford')
     print(f'This car was made by {test_mfg.name} and is the color {test_mfg.color}.')
+    print(test_mfg.summary())
 
 
-    
+
 
