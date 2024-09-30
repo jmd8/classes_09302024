68ffcb926633c7c58c3273ef34946e024963eed8
Jeremy Desiderio <jdesiderio@supplyforce.com>
2024-09-30 14:51:13 -0400

    Created violin class

--- instrument.py
+++ instrument.py
@@ -7,8 +7,13 @@ class Instrument():
     def play_instrument(self):
         return f'Instruments are played {self.played}, produce a {self.sound} sound, and have a {self.timbre} timbre.'
 
+class Violin(Instrument):
+    def __init__(self, played, sound, timbre, bowed):
+        super().__init__(played, sound, timbre)
+        self.bowed = bowed
 
-
+    def play_violin(self):
+        return f'Violins can be played {self.played}, can sound {self.sound}, have a mellow {self.timbre} are usually {self.bowed}.'
 
 
 
@@ -18,7 +23,8 @@ if __name__ == '__main__':
     print("Hello world")
     test_intrument = Instrument('loudly', 'melodious', 'different')
     print(test_intrument.play_instrument())
-    
+    test_violin = Violin('loudly', 'melodious', 'different', 'bowed')
+    print(test_violin.play_violin())
 
 
 
